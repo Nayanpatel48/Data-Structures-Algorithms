c2102781effad64aa3294c090454b721c220fd51
nayan <www.xil187@gmail.com>
2024-07-21 14:56:33 +0530

    insert before and after node which ahs given value (doubly linked list) implemented

--- linkedLists/DoublyLinkedList.kt
+++ linkedLists/DoublyLinkedList.kt
@@ -33,7 +33,6 @@ class LinkedList1<Int>
         //initialize traversal
         var save = head
         var i = 0
-
         //count the number of nodes in the list
         while (save != null)
         {
@@ -88,6 +87,70 @@ class LinkedList1<Int>
         }
         return "$value is added at ending of list successfully!"
     }
+    fun insertAfter(value : Int, valueToBeInserted : Int) : String
+    {
+        //create and initializing new node
+        var newNode = Node1(valueToBeInserted)
+
+        newNode.prev=null
+        newNode.next=null
+
+        //traversing till the value is not found
+        var save = head
+        while (save?.value != value)
+        {
+            save=save?.next
+            if (save==null)
+                break
+        }
+
+        if (save != null) // if value found
+        {
+            newNode.next = save.next
+            newNode.prev=save
+            save.next?.prev= newNode
+            save.next=newNode
+            return "$valueToBeInserted is inserted after $value successfully!"
+        }
+        else
+            return "Node not found"
+    }
+    fun insertBefore(value : Int, valueToBeInserted : Int) : String
+    {
+        //create and initializing new node
+        var newNode = Node1(valueToBeInserted)
+
+        newNode.prev=null
+        newNode.next=null
+
+        //if value is found in first node
+        if (head?.value == valueToBeInserted)
+        {
+            newNode.next = head
+            head!!.prev= newNode
+            head=newNode
+            return "$valueToBeInserted is inserted before $value successfully!!"
+        }
+
+        //traversing linked list till the node containing value not found
+        var save = head
+        while (save?.value != value)
+        {
+            save=save?.next
+            if (save==null)
+                break
+        }
+        if(save!=null)
+        {
+            newNode.next=save
+            newNode.prev=save.prev
+            save.prev?.next=newNode
+            save.prev=newNode
+            return "$valueToBeInserted is inserted before $value successfully!!"
+        }
+        else
+            return "Node not found!"
+    }
 }
 fun main()
 {
@@ -102,9 +165,9 @@ fun main()
         println("Press 2 = count number of nodes")
         println("Press 3 = insert at the beginning")
         println("Press 4 = insert at the ending")
-        println("Press 5 = insert at specific location")
-        println("Press 6 = insert after given node")
-        println("Press 7 = insert before given node")
+        println("Press 5 = insert after given node")
+        println("Press 6 = insert before given node")
+        println("Press 7 = insert at specific location")
         println("Press 8 = delete from beginning")
         println("Press 9 = delete from ending")
         println("Press 10 = delete the specific node")
@@ -136,11 +199,19 @@ fun main()
                 continue
             }
             5 -> {
-
+                println("Enter value:")
+                var x = readln().toInt()
+                println("Enter value to be inserted after $x :")
+                var y = readln().toInt()
+                println(linkedList1.insertAfter(x,y))
                 continue
             }
             6 -> {
-
+                println("Enter value:")
+                var x = readln().toInt()
+                println("Enter value to be inserted before $x :")
+                var y = readln().toInt()
+                println(linkedList1.insertBefore(x,y))
                 continue
             }
             7 -> {
